e7b3ad4edd50ca8341914fd8c0a8c755f258d3c9
joseluisth01 <joseluistirado02@gmail.com>
2024-04-08 16:12:33 +0200

    ULT

--- frontcine/src/componentes/Footer.jsx
+++ frontcine/src/componentes/Footer.jsx
@@ -4,29 +4,41 @@ import '../style/footerstyle.css';
 export const Footer = () => {
   return (
     <div className='divfooter'>
+      <div className="bloque1footer">
         <div className="nuestroscines">
-            <h3>NUESTROS CINES</h3>
-            <p>Tapacos Córdoba</p>
-            <p>Tapacos Sevilla</p>
-            <p>Tapacos Málaga</p>
-            <p>Proximamente...</p>
+          <h3>NUESTROS CINES</h3>
+          <p className='linkfooter'>Tapacos Córdoba</p>
+          <p className='linkfooter'>Tapacos Sevilla</p>
+          <p className='linkfooter'>Tapacos Málaga</p>
+          <p className='linkfooter'>Proximamente...</p>
         </div>
         <div className='infotapacos'>
-            <h3>TAPACOS CINEMAS</h3>
-            <p>Sesiones</p>
-            <p>Ofertas</p>
-            <p>Versiones VOSE</p>
-            <p>Tarjeta Tapacos</p>
-            <p>Proximamente</p>
+          <h3>TAPACOS CINEMAS</h3>
+          <p className='linkfooter'>Sesiones</p>
+          <p className='linkfooter'>Ofertas</p>
+          <p className='linkfooter'>Versiones VOSE</p>
+          <p className='linkfooter'>Tarjeta Tapacos</p>
+          <p className='linkfooter'>Proximamente</p>
         </div>
         <div className='acercadenosotros'>
-            <h3>ACERCA DE NOSOTROS</h3>
-            <p>Contacto</p>
-            <p>Preguntas frecuentes</p>
-            <p>Política de devoluciones</p>
-            <p>Política de privacidad</p>
-            <p>Política de Cookies</p>
+          <h3>ACERCA DE NOSOTROS</h3>
+          <p className='linkfooter'>Contacto</p>
+          <p className='linkfooter'>Preguntas frecuentes</p>
+          <p className='linkfooter'>Política de devoluciones</p>
+          <p className='linkfooter'>Política de privacidad</p>
+          <p className='linkfooter'>Política de Cookies</p>
         </div>
+      </div>
+      <div class="avisoslegales_footer">
+            <p className='linkavisoslegales'>Aviso Legal</p>
+            <p>|</p>
+            <p className='linkavisoslegales'>Política de Privacidad</p>
+            <p>|</p>
+            <p className='linkavisoslegales'>Política de Cookies</p>
+        </div>
+      <div className="partners">
+        <p>Copyright © 2024 TAPACO CINEMAS. Todos los derechos reservados.</p>
+      </div>
     </div>
   )
 }
--- frontcine/src/componentes/Header.jsx
+++ frontcine/src/componentes/Header.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../style/headerestilo.css';
 
+
 export const Header = () => {
-    function cambiarTexto() {
-        var select = document.querySelector('.selectheader');
-        var selectedOption = select.options[select.selectedIndex];
-        if (selectedOption.value !== "") {
-            selectedOption.disabled = true;
-        }
-    }
+    const [usuarioImg, setUsuarioImg] = useState('/img/usuario.png');
+    const usuarioHover = '/img/usuarioverde.png';
+
+    const handleMouseEnter = () => {
+        setUsuarioImg(usuarioHover);
+    };
+
+    const handleMouseLeave = () => {
+        setUsuarioImg('/img/usuario.png');
+    };
 
     return (
         <header className='divheader'>
-            <button class="logoheader" data-text="Awesome">
-                <span class="actual-text">&nbsp;TAPACOS&nbsp;</span>
-                <span aria-hidden="true" class="hover-text">&nbsp;TAPACOS&nbsp;</span>
+            <button className="logoheader" data-text="Awesome">
+                <span className="actual-text">&nbsp;TAPACOS&nbsp;</span>
+                <span aria-hidden="true" className="hover-text">&nbsp;TAPACOS&nbsp;</span>
             </button>
 
-            <select class="selectheader" onchange="cambiarTexto()">
+            <select className="selectheader" onChange={() => cambiarTexto()}>
                 <option disabled selected value="">Selecciona la ciudad</option>
-                <option class='opcion' value="opcion1">TAPACOS CÓRDOBA</option>
-                <option class='opcion' value="opcion2">TAPACOS SEVILLA</option>
-                <option class='opcion' value="opcion2">TAPACOS MÁLAGA</option>
+                <option className='opcion' value="opcion1">TAPACOS CÓRDOBA</option>
+                <option className='opcion' value="opcion2">TAPACOS SEVILLA</option>
+                <option className='opcion' value="opcion2">TAPACOS MÁLAGA</option>
             </select>
 
+            <img 
+                className='usuario' 
+                src={usuarioImg} 
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave} 
+                alt="Usuario" 
+            />
         </header>
     )
 }
