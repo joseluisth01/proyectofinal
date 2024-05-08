bda27d0b6caf37d3f37e3c9563a700bb00338acc
aguadooo2 <jam0018@alu.medac.es>
2024-05-08 19:01:50 +0200

    GARFIEL 3

--- frontcine/src/componentes/Main.jsx
+++ frontcine/src/componentes/Main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import '../style/mainstyle.css';
 import { Slider } from './Slider';
 
-// Asegúrate de tener la imagen en el directorio `public` para poder utilizarla directamente.
-const garfieldImg = '/img/GARFIELD.jpg'; // Reemplaza este con el nombre correcto de tu imagen
+const garfieldImg = '/img/GARFIELD.jpg'; // Ruta de la imagen guardada
 
 export const Main = () => {
   return (
@@ -14,14 +13,25 @@ export const Main = () => {
         <Slider />
         <br /><br /><br /><br /><br />
         <section className='garfield-promotion'>
-          <img src={garfieldImg} alt='Garfield' className='garfield-img' />
           <div className='garfield-content'>
             <h2>Concurso - Garfield: la Película</h2>
             <p>
-              #Garfield, el gato más vago y gracioso llega a Odeon Multicines con regalos muy felinos.
-              ¡Participa y gana un pack de merchandising de la película!
+              <b> #Garfield</b>, el gato más vago y gracioso llega a Odeon Multicines con regalos muy felinos.
+              No hablamos de lasagna, pero os traemos packs formados por un peluche, unos stickers
+              y una camiseta de adulto o niño. 🐱
             </p>
+            <h3>¿Qué tienes que hacer para conseguirlos?</h3>
+            <ul>
+              <li>📌 1. Síguenos.</li>
+              <li>📌 2. Menciona a la persona que te llevarías a verla.</li>
+              <li>📌 3. Haber visto la película en Odeon Multicines y estar en posesión de la entrada.</li>
+            </ul>
+            <p>Tienes hasta el 15 de mayo a las 23:55 horas para participar (huso horario peninsular).</p>
+            <a href='https://epr.ms/3UAodQ4' target='_blank' rel='noopener noreferrer'>
+              Bases legales: https://epr.ms/3UAodQ4
+            </a>
           </div>
+          <img src={garfieldImg} alt='Garfield Promoción' className='garfield-img' />
         </section>
       </main>
       <footer>
