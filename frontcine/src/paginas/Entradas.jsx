97aa7796d660c2bf7c2ccfe09ad95b12a3223ec6
aguadooo2 <jam0018@alu.medac.es>
2024-05-08 20:10:08 +0200

    PAGS

---
+++ frontcine/src/paginas/Entradas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from '../componentes/Header'
+
+export const Entradas = () => {
+    return (
+        <div>
+            <Header />
+            Entradas
+            <br /><br /><br /><br /><br />
+        </div>
+    )
+}
+
