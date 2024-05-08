b75ee6b5e8a99e465d17b1cd13323e126cbe058e
aguadooo2 <jam0018@alu.medac.es>
2024-05-08 18:49:02 +0200

    GARFIELD

--- frontcine/src/componentes/Main.jsx
+++ frontcine/src/componentes/Main.jsx
@@ -1,26 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../style/mainstyle.css';
 import { Slider } from './Slider';
 
+// Asegúrate de tener la imagen en el directorio `public` para poder utilizarla directamente.
+const garfieldImg = '/img/GARFIELD.png'; // Reemplaza este con el nombre correcto de tu imagen
 
 export const Main = () => {
-
-
   return (
     <div className='divmain'>
-
       <header>
-
       </header>
       <main>
         <Slider />
         <br /><br /><br /><br /><br />
-        
+        <section className='garfield-promotion'>
+          <img src={garfieldImg} alt='Garfield' className='garfield-img' />
+          <div className='garfield-content'>
+            <h2>Concurso - Garfield: la Película</h2>
+            <p>
+              #Garfield, el gato más vago y gracioso llega a Odeon Multicines con regalos muy felinos.
+              ¡Participa y gana un pack de merchandising de la película!
+            </p>
+          </div>
+        </section>
       </main>
       <footer>
-
       </footer>
-
     </div>
-  )
-}
+  );
+};
