4aaaa5a683593e606c74f842f2ef5e52cbf9493b
joseluisth01 <joseluistirado02@gmail.com>
2024-05-08 18:08:31 +0200

    wdq

--- frontcine/src/componentes/PeliculasList.jsx
+++ frontcine/src/componentes/PeliculasList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import '../style/peliculasList.css';
 import addDays from 'date-fns/addDays';
 
