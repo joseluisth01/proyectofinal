27abad1fde5919571c4b4f3b2af22042389ee268
aguadooo2 <jam0018@alu.medac.es>
2024-05-29 01:19:55 +0200

    dzfhdfhd

--- frontcine/src/componentes/Main.jsx
+++ frontcine/src/componentes/Main.jsx
@@ -49,56 +49,32 @@ export const Main = () => {
           </div>
         </section>
 
-        <div className="fondoContacto">
-          <div
-            class="max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md
-            before:w-24 before:h-24 before:absolute before:bg-green-600 before:rounded-full before:-z-10 
-            before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-green-400 after:rounded-full 
-            after:-z-10 after:blur-xl after:top-24 after:-right-12
-            bg-black m-5 w-1/2">
-            <h2 class="text-2xl font-bold text-white mb-6">¿Qué te parece TAPACOS?</h2>
+        <div class="fondoContacto">
+        <div class="form-container">
+            <h2 class="form-title">¿Qué te parece TAPACOS?</h2>
 
             <form>
-              <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-300" for="name">Nombre</label>
-                <input
-                  class="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
-                  type="text"
-                />
-              </div>
+                <div class="form-group">
+                    <label class="form-label" for="name">Nombre</label>
+                    <input class="form-input" type="text" id="name" />
+                </div>
 
-              <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-300" for="email">Dirección de correo electrónico</label>
-                <input
-                  class="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
-                  name="email"
-                  id="email"
-                  type="email"
-                />
-              </div>
+                <div class="form-group">
+                    <label class="form-label" for="email">Dirección de correo electrónico</label>
+                    <input class="form-input" name="email" id="email" type="email" />
+                </div>
 
-              <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-300" for="bio">Mensaje</label>
-                <textarea
-                  class="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
-                  rows="3"
-                  name="bio"
-                  id="bio"
-                ></textarea>
-              </div>
+                <div class="form-group">
+                    <label class="form-label" for="bio">Mensaje</label>
+                    <textarea class="form-input" rows="3" name="bio" id="bio"></textarea>
+                </div>
 
-              <div class="flex justify-end">
-                <button
-                  class="bg-gradient-to-r from-gray-900 via-green-400 to-green-600 text-white px-4 py-2 font-bold rounded-md hover:opacity-80"
-                  type="submit"
-                >
-                  Enviar mensaje
-                </button>
-              </div>
+                <div class="form-actions">
+                    <button class="submit-button" type="submit">Enviar mensaje</button>
+                </div>
             </form>
-          </div>
-
         </div>
+    </div>
 
 
         <div class="container">
