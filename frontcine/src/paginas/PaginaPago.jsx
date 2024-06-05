2f937fe7d21e3044a38ad25bb8245f96497d19cf
joseluisth01 <joseluistirado02@gmail.com>
2024-06-06 00:18:16 +0200

    wef

--- frontcine/src/paginas/PaginaPago.jsx
+++ frontcine/src/paginas/PaginaPago.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate, useLocation } from 'react-router-dom';
+
+const hamburguesa = '/img/hamburguesa.png';
+
 import '../style/paginaPago.css';
 
 export const PaginaPago = () => {
@@ -56,32 +59,72 @@ export const PaginaPago = () => {
 
     return (
         <div className='fondo'>
-            <div className='divpaginapago'>
-            <div className="pago-container">
-    <h1>Información de Pago</h1>
-    <div className="form-group">
-        <label>Email:</label>
-        <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
-    </div>
-    <div className="form-group">
-        <label>Número de Tarjeta de Crédito:</label>
-        <input type="text" placeholder="#### #### #### ####" value={tarjeta} onChange={(e) => setTarjeta(e.target.value)} />
-    </div>
-    <div className="form-group">
-        <label>Fecha de Caducidad:</label>
-        <input type="text" placeholder="MM/AA"  />
-    </div>
-    <div className="form-group">
-        <label>CVV:</label>
-        <input type="text" placeholder="CVV" />
-    </div>
-    <div className="form-group">
-        <input type="checkbox" checked={aceptar} onChange={(e) => setAceptar(e.target.checked)} />
-        <label>Acepto los términos y condiciones</label>
-    </div>
-    <button onClick={handlePago}>Pagar</button>
-</div>
+            {/*             <div className='divpaginapago'>
+                <div className="pago-container">
+                    <h1>Información de Pago</h1>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label>Número de Tarjeta de Crédito:</label>
+                        <input type="text" placeholder="#### #### #### ####" value={tarjeta} onChange={(e) => setTarjeta(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label>Fecha de Caducidad:</label>
+                        <input type="text" placeholder="MM/AA" />
+                    </div>
+                    <div className="form-group">
+                        <label>CVV:</label>
+                        <input type="text" placeholder="CVV" />
+                    </div>
+                    <div className="form-group">
+                        <input type="checkbox" checked={aceptar} onChange={(e) => setAceptar(e.target.checked)} />
+                        <label>Acepto los términos y condiciones</label>
+                    </div>
+                    <button onClick={handlePago}>Pagar</button>
+                </div>
 
+            </div> */}
+            <div className='bloqueizq_comida'>
+                <div className='preguntacomida'>
+                    <h1>¿Quieres añadir alguno de estos productos a tu compra?</h1>
+                </div>
+                <div className='comidas'>
+                    <div className="comida1">
+                        <div className="flex3">
+                            <div className='imgcomida'>
+                                <img src={hamburguesa} alt='Garfield Promoción' className='comida-img' />
+                            </div>
+                            <div className='detallescomida'>
+                                <h2 className='nombrecomida'>Menú Burger</h2>
+                                <p class="precioactualcomida">PRECIO ACTUAL 10,00€</p>
+                                <p class="precioanteriorcomida">Precio anterior 13,00€</p>
+                                <p className='descripcioncomida'>
+                                ¡Precio exclusivo WEB! A elegir entre nuestras hamburguesas (Rita burger, Classic burger, Crispy chicken o Veggie burger) con patatas y bebidas incluida.
+                                </p>
+                                <button className="detalles-pelicula-boton mb-6">AÑADIR</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="comida1">
+                        <div className="flex3">
+                            <div className='imgcomida'>
+                                <img src={hamburguesa} alt='Garfield Promoción' className='comida-img' />
+                            </div>
+                            <div className='detallescomida'>
+                                <h2 className='nombrecomida'>Menú Burger</h2>
+                                <p class="precioactualcomida">PRECIO ACTUAL 10,00€</p>
+                                <p class="precioanteriorcomida">Precio anterior 13,00€</p>
+                                <p className='descripcioncomida'>
+                                ¡Precio exclusivo WEB! A elegir entre nuestras hamburguesas (Rita burger, Classic burger, Crispy chicken o Veggie burger) con patatas y bebidas incluida. Opción sin gluten disponible.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
 
