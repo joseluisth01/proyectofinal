82323d2285565655be04f8fd787aa1b5c7063bdd
joseluisth01 <joseluistirado02@gmail.com>
2024-06-08 17:10:29 +0200

    werc

--- frontcine/src/componentes/Footer.jsx
+++ frontcine/src/componentes/Footer.jsx
@@ -8,36 +8,41 @@ export const Footer = () => {
     <div className='divfooter'>
       <div className="bloque1footer">
         <div className="nuestroscines">
-          <h3>NUESTROS CINES</h3>
+          <a href='/' className="logoheader" data-text="Awesome">
+            <span className="actual-text">&nbsp;TAPACOS</span>
+            <span aria-hidden="true" className="hover-text">&nbsp;&nbsp;</span>
+          </a>
+          {/* <h3>NUESTROS CINES</h3>
           <p className='linkfooter'>Tapacos Córdoba</p>
           <p className='linkfooter'>Tapacos Sevilla</p>
           <p className='linkfooter'>Tapacos Málaga</p>
-          <p className='linkfooter'>Proximamente...</p>
+          <p className='linkfooter'>Proximamente...</p> */}
         </div>
         <div className='infotapacos'>
-          <h3>TAPACOS CINEMAS</h3>
-          <Link to='/Cartelera' className='linkfooter'>Cartelera</Link>
-          <p className='linkfooter'>Ofertas</p>
-          <p className='linkfooter'>VOSE</p>
-          <p className='linkfooter'>Tarjeta Socio Tapacos</p>
+          <h3 className="titfooter">TAPACOS CINEMAS</h3>
+          <Link to='/Cartelera' className='linkfooter'><p>🡆 Cartelera</p></Link>
+          <p className='linkfooter'>🡆 Ofertas</p>
+          <p className='linkfooter'>🡆 VOSE</p>
+          <p className='linkfooter'>🡆 Tarjeta Socio Tapacos</p>
         </div>
         <div className='acercadenosotros'>
-          <h3>ACERCA DE NOSOTROS</h3>
-          <p className='linkfooter'>Contacto</p>
-          <p className='linkfooter'>Quienes somos</p>
-          <p className='linkfooter'>Preguntas frecuentes</p>
-          <p className='linkfooter'>Política de devoluciones</p>
+          <h3 className="titfooter">ACERCA DE NOSOTROS</h3>
+          <p className='linkfooter'>🡆 Contacto</p>
+          <p className='linkfooter'>🡆 Quienes somos</p>
+          <p className='linkfooter'>🡆 Preguntas frecuentes</p>
+          <p className='linkfooter'>🡆 Política de devoluciones</p>
         </div>
+
       </div>
       <div class="avisoslegales_footer">
-            <Link to='/AvisoLegal' className='linkavisoslegales'>Aviso Legal</Link>
-            <p>|</p>
-            <Link to='/PoliticaPrivacidad' className='linkavisoslegales'>Política de Privacidad</Link>
-            <p>|</p>
-            <Link to='/PoliticaCookies' className='linkavisoslegales'>Política de Cookies</Link>
-        </div>
+        <Link to='/AvisoLegal' className='linkavisoslegales'>Aviso Legal</Link>
+        <p>|</p>
+        <Link to='/PoliticaPrivacidad' className='linkavisoslegales'>Política de Privacidad</Link>
+        <p>|</p>
+        <Link to='/PoliticaCookies' className='linkavisoslegales'>Política de Cookies</Link>
+      </div>
       <div className="partners">
-        <Link to='/Cartelera'>Copyright © 2024 TAPACO CINEMAS. Todos los derechos reservados.</Link>
+        <p>Copyright © 2024 TAPACO CINEMAS. Todos los derechos reservados.</p>
       </div>
     </div>
   )
